f27b5a3c69c15cdf46895604fd20fac66cd3deeb
lytranuit <lytranuit@gmail.com>
2021-08-12 16:31:38 +0700

    update

--- app/Views/frontend/widget/sidebar/product.php
+++ app/Views/frontend/widget/sidebar/product.php
@@ -1,4 +1,4 @@
-<div>
+<div class="text-center">
     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFoodzoneofSimba%2F&tabs=timeline&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=724049858115617" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 </div>
 
